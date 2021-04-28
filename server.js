dcef85c0b7033f21a6ca3631890b0a45b1d20c0a
Corey <corey@oak.works>
2021-04-28 17:25:14 -0400

    server.js page now launches react

---
+++ server.js
@@ -0,0 +1,25 @@
+//remembered
+const express = require("express");
+// const mongoose = require("mongoose");
+const logger = require("morgan");
+//had to look up
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/myLibraryDB", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => console.log("listening on port:" + PORT))
