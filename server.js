eb5d815be1445c987578b8ebdcc34cee2fd53b7d
Corey <corey@oak.works>
2021-05-01 15:02:45 -0400

    created front-end file structure

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-//remembered
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const logger = require("morgan");
-//had to look up
 const path = require("path");
-
-const PORT = process.env.PORT || 3001;
+require("dotenv").config()
+const PORT = process.env.PORT || 3002;
 
 const app = express();
 
@@ -13,7 +11,7 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/myLibraryDB", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGODB_URI2 || "mongodb://localhost:27017/myLibraryDB", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
